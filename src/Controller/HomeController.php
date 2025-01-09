dfc0b431bcfe310da187795217ab7c3cef37d86f
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-09 14:31:06 +0100

    auto logged after register and login working

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,20 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\BookReadRepository;
+use App\Security\LoginAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
-    private BookReadRepository $readBookRepository;
+    private BookReadRepository $bookReadRepository;
 
     // Inject the repository via the constructor
     public function __construct(BookReadRepository $bookReadRepository)
@@ -38,26 +40,36 @@ class HomeController extends AbstractController
 
 
     #[Route('/login', name: 'auth.login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('auth/login.html.twig', [
-            'name' => 'Thibaud',
-        ]);
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app.home');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route('/register', name: 'auth.register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        Security $security,
+        EntityManagerInterface $entityManager,
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $password = $form->get('password')->getData();
-            $confirmPassword = $form->get('confirmPassword')->getData();
+            $confirmPasswordField = $form->get('confirmPassword');
+            $confirmPassword = $confirmPasswordField->getData();
 
             if ($password !== $confirmPassword) {
-                $form->get('confirmPassword')->addError(new FormError('Les mots de passe sont différents.'));
+                $confirmPasswordField->addError(new FormError('Les mots de passe sont différents.'));
                 return $this->render('auth/register.html.twig', [
                     'form' => $form,
                 ]);
@@ -69,6 +81,8 @@ class HomeController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            $security->login($user, LoginAuthenticator::class, 'main');
+
             return $this->redirectToRoute('app.home');
         }
 
@@ -76,4 +90,7 @@ class HomeController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route(path: '/logout', name: 'auth.logout')]
+    public function logout(): void {}
 }
